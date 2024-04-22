3038af6e46e9f30f3a86973bc47ce8a28b69fec7
HDN <hdn@oporko.com>
2024-04-22 19:03:31 -0400

    Improve find_targets test

--- tasks/tests/test_find_targets.py
+++ tasks/tests/test_find_targets.py
@@ -1,6 +1,7 @@
 import unittest
 import tasks.tests.util as testutil
 from invoke import MockContext
+from pathlib import Path
 from tasks.build import find_targets
 
 class FindTargetsTest(unittest.TestCase):
@@ -51,6 +52,41 @@ class FindTargetsTest(unittest.TestCase):
         find_targets(self.c)
 
         self.assertEqual(18, len(self.c.build.targets))
+
+        self.assert_target('ai')
+        self.assert_target('aiMeshes')
+        self.assert_target('animations')
+        self.assert_target('font')
+        self.assert_target('menu')
+        self.assert_target('menu', extension='_001')
+        self.assert_target('objects')
+        self.assert_target('shaders')
+        self.assert_target('sound')
+        self.assert_target('sound', extension='_001')
+        self.assert_target('standardMesh')
+        self.assert_target('standardMesh', extension='_001')
+        self.assert_target('texture')
+        self.assert_target('texture', extension='_001')
+        self.assert_target('treeMesh')
+        self.assert_target('game', base_path=Path('bf1942', 'game'))
+        self.assert_target('level1', base_path=Path('bf1942', 'levels', 'level1'))
+        self.assert_target('level2', base_path=Path('bf1942', 'levels', 'level2'))
+
+    def assert_target(self, name, base_path=None, extension=None):
+        full_name = name
+        if extension is not None:
+            full_name += extension
+
+        if base_path is None:
+            base_path = Path(name)
+
+        found = [t for t in self.c.build.targets if t.full_name == full_name]
+        target = next(iter(found), None)
+
+        self.assertIsNotNone(target, f'Target {full_name} not found')
+        self.assertEqual(target.name, name)
+        self.assertEqual(target.extension, extension)
+        self.assertEqual(target.base_path, base_path)
 
 if __name__ == '__main__':
     unittest.main()
